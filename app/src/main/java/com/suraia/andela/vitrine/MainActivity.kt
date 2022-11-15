025312136dc3323019d265c155da370fc1ba72d7
Suraia <suraiafragoso@gmail.com>
2022-11-15 09:29:08 +0100

    first commit

---
+++ app/src/main/java/com/suraia/andela/vitrine/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.suraia.andela.vitrine
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
